90eeed8adc66a2df6d84e0c3a61bf64ae69c193e
IAmShridharr <shridharpandeyofficial@gmail.com>
2023-05-10 09:19:39 +0530

    Admin profile first draft

--- app/src/main/java/com/infiniterealm/achievers/admins/mainFragments/AdminProfileFragment.java
+++ app/src/main/java/com/infiniterealm/achievers/admins/mainFragments/AdminProfileFragment.java
@@ -13,8 +13,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.infiniterealm.achievers.R;
